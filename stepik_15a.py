c355e47aad5b5e48f29b738b63c8afff08b27908
Amelia Hajkowska <ameliahajkowska@gmail.com>
2024-01-24 20:08:35 +0100

    update zad3

--- stepik_15a.py
+++ stepik_15a.py
@@ -4,3 +4,17 @@ def iloczyn(a, b):
 
 def suma(a, b):
     return a + b
+
+import unittest
+
+class TestFunctions(unittest.TestCase):
+    def test_iloczyn(self):
+        result = iloczyn(2, 3)
+        self.assertEqual(result, 6)
+
+    def test_suma(self):
+        result = suma(2, 3)
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
