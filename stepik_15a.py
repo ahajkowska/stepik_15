2854eb5f5e52cd907f6c2ee94406504574b1a54a
Amelia Hajkowska <ameliahajkowska@gmail.com>
2024-01-24 20:29:58 +0100

    it works

--- stepik_15a.py
+++ stepik_15a.py
@@ -4,3 +4,11 @@ def iloczyn(a, b):
 
 def suma(a, b):
     return a + b
+
+def test_iloczyn():
+    result = iloczyn(2, 3)
+    assert result == 6
+
+def test_suma():
+    result = suma(2, 3)
+    assert result == 5
